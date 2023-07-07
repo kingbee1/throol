717187ad48687f03a37286cca657130594915e7e
Abiodun <abiodunhodonu@gmail.com>
2023-07-07 18:02:32 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+
+function App() {
+
+
+  return (
+      <div className='test'>
+        <form >
+          <input type="text" />
+          <button type="submit">submit</button>
+        </form>
+      </div>
+
+  )
+}
+
+export default App
